c89fb948c72895b146f93ecef3e6de8a0be40ae3
vhavlena <vohavlena@gmail.com>
2023-02-08 16:17:22 +0100

    dec-proc: tests; changed interface

--- src/test/noodler/decision-procedure.cpp
+++ src/test/noodler/decision-procedure.cpp
@@ -15,15 +15,15 @@ public:
     TheoryStrNoodlerCUT(smt::context &ctx, ast_manager &m, const theory_str_params &params)
             : theory_str_noodler(ctx, m, params) {}
 
-    using theory_str_noodler::m_util_s, theory_str_noodler::m;
+    using theory_str_noodler::m_util_s, theory_str_noodler::m, theory_str_noodler::m_util_a;
 };
 
 class DecisionProcedureCUT : public DecisionProcedure {
 public:
     DecisionProcedureCUT(const Formula &equalities, AutAssignment init_aut_ass,
                          const std::unordered_set<BasicTerm>& init_length_sensitive_vars,
-                         ast_manager& m, seq_util& m_util_s
-    ) : DecisionProcedure(equalities, std::move(init_aut_ass), init_length_sensitive_vars, m, m_util_s) {}
+                         ast_manager& m, seq_util& m_util_s, arith_util& m_util_a
+    ) : DecisionProcedure(equalities, std::move(init_aut_ass), init_length_sensitive_vars, m, m_util_s, m_util_a) {}
 
     using DecisionProcedure::compute_next_solution;
     using DecisionProcedure::get_lengths;
@@ -54,6 +54,17 @@ static std::shared_ptr<Mata::Nfa::Nfa> regex_to_nfa(const std::string& regex) {
     return std::make_shared<Mata::Nfa::Nfa>(aut);
 }
 
+static std::map<BasicTerm, expr_ref> create_var_map(const std::unordered_set<BasicTerm>& vars, ast_manager& m, seq_util& m_util_s) {
+    std::map<BasicTerm, expr_ref> ret;
+    
+    for(const BasicTerm& v : vars) {
+        expr_ref var(m_util_s.mk_skolem(symbol(v.get_name()), 0, nullptr, m_util_s.mk_string_sort()), m);
+        ret.insert({v, var});
+    }
+
+    return ret;
+}
+
 TEST_CASE("Decision Procedure", "[noodler]") {
     memory::initialize(0);
     smt_params params;
@@ -63,6 +74,7 @@ TEST_CASE("Decision Procedure", "[noodler]") {
     theory_str_params str_params{};
     TheoryStrNoodlerCUT noodler{ctx, ast_m, str_params };
     auto& m_util_s{ noodler.m_util_s };
+    auto& m_util_a{ noodler.m_util_a };
     auto& m{ noodler.m };
 
     SECTION("unsat-simple", "[nooodler]") {
@@ -74,7 +86,7 @@ TEST_CASE("Decision Procedure", "[noodler]") {
         init_ass[get_var('y')] = regex_to_nfa("a*");
         init_ass[get_var('z')] = regex_to_nfa("b");
         init_ass[get_var('u')] = regex_to_nfa("b*");
-        DecisionProcedureCUT proc(equalities, init_ass, { }, m, m_util_s);
+        DecisionProcedureCUT proc(equalities, init_ass, { }, m, m_util_s, m_util_a);
         CHECK(!proc.compute_next_solution());
     }
 
@@ -87,7 +99,7 @@ TEST_CASE("Decision Procedure", "[noodler]") {
         init_ass[get_var('y')] = regex_to_nfa("a*");
         init_ass[get_var('z')] = regex_to_nfa("a*");
         init_ass[get_var('u')] = regex_to_nfa("a*");
-        DecisionProcedureCUT proc(equalities, init_ass, { }, m, m_util_s);
+        DecisionProcedureCUT proc(equalities, init_ass, { }, m, m_util_s, m_util_a);
         CHECK(proc.compute_next_solution()); 
     }
 
@@ -99,7 +111,7 @@ TEST_CASE("Decision Procedure", "[noodler]") {
         init_ass[get_var('x')] = regex_to_nfa("a*");
         init_ass[get_var('y')] = regex_to_nfa("a+b+");
         init_ass[get_var('z')] = regex_to_nfa("b*");
-        DecisionProcedureCUT proc(equalities, init_ass, { }, m, m_util_s);
+        DecisionProcedureCUT proc(equalities, init_ass, { }, m, m_util_s, m_util_a);
         CHECK(!proc.compute_next_solution());
     }
 
@@ -112,7 +124,7 @@ TEST_CASE("Decision Procedure", "[noodler]") {
         init_ass[get_var('y')] = regex_to_nfa("a*");
         init_ass[get_var('z')] = regex_to_nfa("b");
         init_ass[get_var('u')] = regex_to_nfa("b*");
-        DecisionProcedureCUT proc(equalities, init_ass, { BasicTerm(BasicTermType::Variable, "x"), BasicTerm(BasicTermType::Variable, "z") }, m, m_util_s);
+        DecisionProcedureCUT proc(equalities, init_ass, { BasicTerm(BasicTermType::Variable, "x"), BasicTerm(BasicTermType::Variable, "z") }, m, m_util_s, m_util_a);
         CHECK(!proc.compute_next_solution());
     }
 
@@ -125,7 +137,7 @@ TEST_CASE("Decision Procedure", "[noodler]") {
         init_ass[get_var('y')] = regex_to_nfa("a*");
         init_ass[get_var('z')] = regex_to_nfa("a*");
         init_ass[get_var('u')] = regex_to_nfa("a*");
-        DecisionProcedureCUT proc(equalities, init_ass, { BasicTerm(BasicTermType::Variable, "x"), BasicTerm(BasicTermType::Variable, "z") }, m, m_util_s);
+        DecisionProcedureCUT proc(equalities, init_ass, { BasicTerm(BasicTermType::Variable, "x"), BasicTerm(BasicTermType::Variable, "z") }, m, m_util_s, m_util_a);
         CHECK(proc.compute_next_solution());
         CHECK(proc.compute_next_solution());
         CHECK(!proc.compute_next_solution());
@@ -140,7 +152,7 @@ TEST_CASE("Decision Procedure", "[noodler]") {
         init_ass[get_var('y')] = regex_to_nfa("ab*");
         init_ass[get_var('z')] = regex_to_nfa("ab*");
         init_ass[get_var('u')] = regex_to_nfa("a*");
-        DecisionProcedureCUT proc(equalities, init_ass, { BasicTerm(BasicTermType::Variable, "x"), BasicTerm(BasicTermType::Variable, "z") }, m, m_util_s);
+        DecisionProcedureCUT proc(equalities, init_ass, { BasicTerm(BasicTermType::Variable, "x"), BasicTerm(BasicTermType::Variable, "z") }, m, m_util_s, m_util_a);
 
         REQUIRE(proc.compute_next_solution());
 
@@ -170,7 +182,7 @@ TEST_CASE("Decision Procedure", "[noodler]") {
         init_ass[get_var('z')] = regex_to_nfa("b");
         init_ass[get_var('u')] = regex_to_nfa("b*");
         init_ass[get_var('r')] = regex_to_nfa("a*");
-        DecisionProcedureCUT proc(equalities, init_ass, { }, m, m_util_s);
+        DecisionProcedureCUT proc(equalities, init_ass, { }, m, m_util_s, m_util_a);
         CHECK(!proc.compute_next_solution());
     }
 
@@ -184,7 +196,7 @@ TEST_CASE("Decision Procedure", "[noodler]") {
         init_ass[get_var('z')] = regex_to_nfa("a*");
         init_ass[get_var('u')] = regex_to_nfa("(a|b)*");
         init_ass[get_var('r')] = regex_to_nfa("aaa");
-        DecisionProcedureCUT proc(equalities, init_ass, { }, m, m_util_s);
+        DecisionProcedureCUT proc(equalities, init_ass, { }, m, m_util_s, m_util_a);
         CHECK(proc.compute_next_solution());
     }
 
@@ -198,7 +210,7 @@ TEST_CASE("Decision Procedure", "[noodler]") {
         init_ass[get_var('z')] = regex_to_nfa("b");
         init_ass[get_var('u')] = regex_to_nfa("b*");
         init_ass[get_var('r')] = regex_to_nfa("a*");
-        DecisionProcedureCUT proc(equalities, init_ass, { BasicTerm(BasicTermType::Variable, "x"), BasicTerm(BasicTermType::Variable, "z") }, m, m_util_s);
+        DecisionProcedureCUT proc(equalities, init_ass, { BasicTerm(BasicTermType::Variable, "x"), BasicTerm(BasicTermType::Variable, "z") }, m, m_util_s, m_util_a);
         CHECK(!proc.compute_next_solution());
     }
 
@@ -212,13 +224,27 @@ TEST_CASE("Decision Procedure", "[noodler]") {
         init_ass[get_var('z')] = regex_to_nfa("ab*");
         init_ass[get_var('u')] = regex_to_nfa("a*");
         init_ass[get_var('r')] = regex_to_nfa("ab*a");
-        DecisionProcedureCUT proc(equalities, init_ass, { BasicTerm(BasicTermType::Variable, "x"), BasicTerm(BasicTermType::Variable, "z") }, m, m_util_s);
+        DecisionProcedureCUT proc(equalities, init_ass, { BasicTerm(BasicTermType::Variable, "x"), BasicTerm(BasicTermType::Variable, "z") }, m, m_util_s, m_util_a);
 
         REQUIRE(proc.compute_next_solution());
 
         AutAssignment squashed_aut_ass = proc.solution.flatten_substition_map();
         CHECK(Mata::Nfa::are_equivalent(*squashed_aut_ass.at(get_var('x')), *regex_to_nfa("a")));
         CHECK(Mata::Nfa::are_equivalent(*squashed_aut_ass.at(get_var('z')), *regex_to_nfa("a")));
+
+        auto l_vars = { BasicTerm(BasicTermType::Variable, "x"), BasicTerm(BasicTermType::Variable, "z") };
+        auto var_map = create_var_map(l_vars, m, m_util_s);
+
+        // Not sure now, how to test it
+        //std::cout << mk_pp(proc.get_lengths(var_map), m) << std::endl;
+        expr_ref res(m.mk_and(m.mk_true(), m.mk_and(
+            m.mk_or(
+                m.mk_false(), 
+                m.mk_eq(m_util_s.str.mk_length(var_map.at(BasicTerm(BasicTermType::Variable, "x"))), m_util_a.mk_int(1))),  
+            m.mk_or(m.mk_false(), 
+            m.mk_eq(m_util_s.str.mk_length(var_map.at(BasicTerm(BasicTermType::Variable, "z"))), m_util_a.mk_int(1))) )), 
+        m);
+        CHECK(proc.get_lengths(var_map) == res); // this is false, but syntactically it is the same
 
         CHECK(!proc.compute_next_solution());
     }
@@ -233,7 +259,7 @@ TEST_CASE("Decision Procedure", "[noodler]") {
         init_ass[get_var('z')] = regex_to_nfa("ab*");
         init_ass[get_var('u')] = regex_to_nfa("a*");
         init_ass[get_var('r')] = regex_to_nfa("ab*a");
-        DecisionProcedureCUT proc(equalities, init_ass, { BasicTerm(BasicTermType::Variable, "x"), BasicTerm(BasicTermType::Variable, "z") , BasicTerm(BasicTermType::Variable, "r") }, m, m_util_s);
+        DecisionProcedureCUT proc(equalities, init_ass, { BasicTerm(BasicTermType::Variable, "x"), BasicTerm(BasicTermType::Variable, "z") , BasicTerm(BasicTermType::Variable, "r") }, m, m_util_s, m_util_a);
 
         REQUIRE(proc.compute_next_solution());
 
@@ -251,7 +277,7 @@ TEST_CASE("Decision Procedure", "[noodler]") {
         // equalities.add_predicate(create_equality("yx", "r"));
         AutAssignment init_ass;
         init_ass[get_var('x')] = regex_to_nfa("aa?b*");
-        DecisionProcedureCUT proc(equalities, init_ass, { }, m, m_util_s);
+        DecisionProcedureCUT proc(equalities, init_ass, { }, m, m_util_s, m_util_a);
         CHECK(!proc.compute_next_solution());
     }
 
@@ -261,7 +287,7 @@ TEST_CASE("Decision Procedure", "[noodler]") {
         // equalities.add_predicate(create_equality("yx", "r"));
         AutAssignment init_ass;
         init_ass[get_var('x')] = regex_to_nfa("a*b*");
-        DecisionProcedureCUT proc(equalities, init_ass, { }, m, m_util_s);
+        DecisionProcedureCUT proc(equalities, init_ass, { }, m, m_util_s, m_util_a);
         CHECK(proc.compute_next_solution());
     }
 }
