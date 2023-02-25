80398cd92572e8dc574d17aa87480a466f6e8d17
David Chocholatý <chocholaty.david@protonmail.com>
2023-02-25 15:22:50 +0100

    Remove deprecated '_hex' suffix from function name

--- src/smt/theory_str_noodler/util.cc
+++ src/smt/theory_str_noodler/util.cc
@@ -252,15 +252,9 @@ namespace smt::noodler::util {
 
         // Assign sigma start automata for all yet unassigned variables.
         Mata::OnTheFlyAlphabet mata_alphabet{};
-        std::stringstream string_stream;
-        string_stream << std::hex;
         std::string hex_symbol_string;
         for (const uint32_t symbol : noodler_alphabet) {
-            string_stream << symbol;
-            string_stream >> hex_symbol_string;
-            hex_symbol_string = "\\x{" + hex_symbol_string + "}";
-            mata_alphabet.add_new_symbol(hex_symbol_string, symbol);
-            string_stream.clear();
+            mata_alphabet.add_new_symbol(std::to_string(symbol), symbol);
         }
 
         const Nfa nfa_sigma_star{ Mata::Nfa::create_sigma_star_nfa(&mata_alphabet) };
@@ -376,23 +370,23 @@ namespace smt::noodler::util {
         return regex;
     }
 
-    [[nodiscard]] Nfa conv_to_nfa_hex(const app *expr, const seq_util& m_util_s, const ast_manager& m,
-                                      const std::set<uint32_t>& alphabet, bool make_complement) {
+    [[nodiscard]] Nfa conv_to_nfa(const app *expr, const seq_util& m_util_s, const ast_manager& m,
+                                  const std::set<uint32_t>& alphabet, bool make_complement) {
         Nfa nfa{};
 
         if (m_util_s.re.is_to_re(expr)) { // Handle conversion of to regex function call.
             // Assume that expression inside re.to_re() function is a string of characters.
             SASSERT(expr->get_num_args() == 1);
             const auto arg{ expr->get_arg(0) };
-            nfa = conv_to_nfa_hex(to_app(arg), m_util_s, m, alphabet);
+            nfa = conv_to_nfa(to_app(arg), m_util_s, m, alphabet);
         } else if (m_util_s.re.is_concat(expr)) { // Handle concatenation.
             SASSERT(expr->get_num_args() == 2);
             const auto left{ expr->get_arg(0) };
             const auto right{ expr->get_arg(1) };
             SASSERT(is_app(left));
             SASSERT(is_app(right));
-            auto first_nfa{ conv_to_nfa_hex(to_app(left), m_util_s, m, alphabet) };
-            auto second_nfa{ conv_to_nfa_hex(to_app(right), m_util_s, m, alphabet) };
+            auto first_nfa{ conv_to_nfa(to_app(left), m_util_s, m, alphabet) };
+            auto second_nfa{ conv_to_nfa(to_app(right), m_util_s, m, alphabet) };
             nfa = Mata::Nfa::concatenate(first_nfa, second_nfa);
         } else if (m_util_s.re.is_antimirov_union(expr)) { // Handle Antimirov union.
             assert(false && "re.is_antimirov_union(expr)");
@@ -430,9 +424,9 @@ namespace smt::noodler::util {
             SASSERT(expr->get_num_args() == 1);
             const auto child{ expr->get_arg(0) };
             SASSERT(is_app(child));
-            nfa = conv_to_nfa_hex(to_app(child), m_util_s, m, alphabet);
+            nfa = conv_to_nfa(to_app(child), m_util_s, m, alphabet);
+            nfa.unify_initial();
             for (const auto& initial : nfa.initial) {
-                // FIXME: Cannot that introduce errors if there are transitions leading to the initial states?
                 nfa.final.add(initial);
             }
         } else if (m_util_s.re.is_range(expr)) { // Handle range.
@@ -445,13 +439,13 @@ namespace smt::noodler::util {
             const auto right{ expr->get_arg(1) };
             SASSERT(is_app(left));
             SASSERT(is_app(right));
-            nfa = Mata::Nfa::uni(conv_to_nfa_hex(to_app(left), m_util_s, m, alphabet),
-                                 conv_to_nfa_hex(to_app(right), m_util_s, m, alphabet));
+            nfa = Mata::Nfa::uni(conv_to_nfa(to_app(left), m_util_s, m, alphabet),
+                                 conv_to_nfa(to_app(right), m_util_s, m, alphabet));
         } else if (m_util_s.re.is_star(expr)) { // Handle star iteration.
             SASSERT(expr->get_num_args() == 1);
             const auto child{ expr->get_arg(0) };
             SASSERT(is_app(child));
-            nfa = conv_to_nfa_hex(to_app(child), m_util_s, m, alphabet);
+            nfa = conv_to_nfa(to_app(child), m_util_s, m, alphabet);
             for (const auto& final : nfa.final) {
                 for (const auto& initial : nfa.initial) {
                     nfa.delta.add(final, Mata::Nfa::EPSILON, initial);
@@ -466,7 +460,7 @@ namespace smt::noodler::util {
             SASSERT(expr->get_num_args() == 1);
             const auto child{ expr->get_arg(0) };
             SASSERT(is_app(child));
-            nfa = conv_to_nfa_hex(to_app(child), m_util_s, m, alphabet);
+            nfa = conv_to_nfa(to_app(child), m_util_s, m, alphabet);
             for (const auto& final : nfa.final) {
                 for (const auto& initial : nfa.initial) {
                     nfa.delta.add(final, Mata::Nfa::EPSILON, initial);
@@ -482,7 +476,10 @@ namespace smt::noodler::util {
 
         // Whether to create complement of the final automaton.
         if (make_complement) {
-            Mata::OnTheFlyAlphabet mata_alphabet{ Mata::Nfa::create_alphabet(nfa) };
+            Mata::OnTheFlyAlphabet mata_alphabet{};
+            for (const auto& symbol : alphabet) {
+                mata_alphabet.add_new_symbol(std::to_string(symbol), symbol);
+            }
             nfa = Mata::Nfa::complement(nfa, mata_alphabet);
         }
         return nfa;
