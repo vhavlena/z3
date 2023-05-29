499446fe02aa3c1d178e7a1575a37abe24ce3cd3
vhavlena <vohavlena@gmail.com>
2023-05-29 22:54:51 +0200

    better var names

--- src/smt/theory_str_noodler/theory_str_noodler.cpp
+++ src/smt/theory_str_noodler/theory_str_noodler.cpp
@@ -1550,8 +1550,8 @@ namespace smt::noodler {
             // offset >= |t| && offset <= |t| && s = eps -> indexof = offset
             add_axiom({~offset_ge_len, ~offset_le_len, ~s_eq_empty, i_eq_offset});
 
-            expr_ref x = mk_str_var("index_left");
-            expr_ref y = mk_str_var("index_right");
+            expr_ref x = mk_str_var("index_left_off");
+            expr_ref y = mk_str_var("index_right_off");
             expr_ref xy(m_util_s.str.mk_concat(x, y), m);
             string_theory_propagation(xy);
 
