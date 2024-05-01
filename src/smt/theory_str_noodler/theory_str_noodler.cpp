da892c5c24a5287203180d7eb8d8a61b553ea29f
jurajsic <juraj.sic@outlook.com>
2024-05-01 14:15:59 +0200

    sort by regex loop size and not by automata size

--- src/smt/theory_str_noodler/theory_str_noodler.cpp
+++ src/smt/theory_str_noodler/theory_str_noodler.cpp
@@ -840,38 +840,66 @@ namespace smt::noodler {
             if (is_possible) {
                 STRACE("str", tout << "trying multiple regex membership heuristic" << std::endl;);
                 regex::Alphabet alph(get_symbols_from_relevant());
-                std::map<BasicTerm, std::vector<std::pair<bool,std::shared_ptr<mata::nfa::Nfa>>>> var_to_list_of_automata_and_complement_flag;
+                std::map<BasicTerm, std::vector<std::pair<bool,app*>>> var_to_list_of_regexes_and_complement_flag;
+
+                std::function<unsigned(app*)> get_loop_sum;
+                get_loop_sum = [this, &get_loop_sum](app* reg) -> unsigned {
+                    expr* body;
+                    unsigned lo, hi;
+                    if (this->m_util_s.re.is_loop(reg, body, lo, hi)) {
+                        unsigned body_loop = get_loop_sum(to_app(body));
+                        if (body_loop == 0) {
+                            return hi;
+                        } else {
+                            return hi*body_loop;
+                        }
+                    } else if (this->m_util_s.str.is_string(reg)) {
+                        return 0;
+                    } else {
+                        unsigned sum = 0;
+                        for (unsigned arg_num = 0; arg_num < reg->get_num_args(); ++arg_num) {
+                            sum += get_loop_sum(to_app(reg->get_arg(arg_num)));
+                        }
+                        return sum;
+                    }
+                };
+
+                // TODO instead of building automata now (without doing complement)+sorting by their size, we should sort the regexes (by their expected complexity) and then build automata (import for loop, it can time out)
                 for (const auto &membership: m_membership_todo_rel) {
                     BasicTerm var(BasicTermType::Variable, to_app(std::get<0>(membership))->get_decl()->get_name().str());
-                    std::shared_ptr<mata::nfa::Nfa> nfa = std::make_shared<mata::nfa::Nfa>(regex::conv_to_nfa(to_app(std::get<1>(membership)), m_util_s, m, alph, false, false));
-                    var_to_list_of_automata_and_complement_flag[var].push_back(std::make_pair(!std::get<2>(membership), nfa));
+                    app* reg = to_app(std::get<1>(membership));
+                    var_to_list_of_regexes_and_complement_flag[var].push_back(std::make_pair(!std::get<2>(membership), reg));
                 }
 
-                for (const auto &diseq : m_word_diseq_todo_rel) {
+                for (const auto& diseq : m_word_diseq_todo_rel) {
                     BasicTerm var(BasicTermType::Variable, to_app(diseq.first)->get_decl()->get_name().str());
-                    std::shared_ptr<mata::nfa::Nfa> nfa = std::make_shared<mata::nfa::Nfa>(regex::conv_to_nfa(to_app(diseq.second), m_util_s, m, alph, false, false));
-                    var_to_list_of_automata_and_complement_flag[var].push_back(std::make_pair(true, nfa));
+                    app* reg = to_app(diseq.second);
+                    var_to_list_of_regexes_and_complement_flag[var].push_back(std::make_pair(true, reg));
                 }
 
-                for (const auto &eq : m_word_eq_todo_rel) {
+                for (const auto& eq : m_word_eq_todo_rel) {
                     BasicTerm var(BasicTermType::Variable, to_app(eq.first)->get_decl()->get_name().str());
-                    std::shared_ptr<mata::nfa::Nfa> nfa = std::make_shared<mata::nfa::Nfa>(regex::conv_to_nfa(to_app(eq.second), m_util_s, m, alph, false, false));
-                    var_to_list_of_automata_and_complement_flag[var].push_back(std::make_pair(false, nfa));
+                    app* reg = to_app(eq.second);
+                    var_to_list_of_regexes_and_complement_flag[var].push_back(std::make_pair(false, reg));
                 }
 
-                for (auto & [var, list_of_automata] : var_to_list_of_automata_and_complement_flag) {
-                    std::sort(list_of_automata.begin(), list_of_automata.end(), [](const std::pair<bool,std::shared_ptr<mata::nfa::Nfa>>& l, const std::pair<bool,std::shared_ptr<mata::nfa::Nfa>>& r) {
-                        return ((!l.first && r.first) | (l.second->num_of_states() < r.second->num_of_states()));
+                for (auto& [var, list_of_regexes] : var_to_list_of_regexes_and_complement_flag) {
+                    std::sort(list_of_regexes.begin(), list_of_regexes.end(), [&get_loop_sum](const std::pair<bool,app*>& l, const std::pair<bool,app*>& r) {
+                        return ((!l.first && r.first) | (get_loop_sum(l.second) < get_loop_sum(r.second)));
                     });
-                    std::shared_ptr<mata::nfa::Nfa> intersection = nullptr;
-                    for (auto & [is_complement, nfa] : list_of_automata) {
-                        STRACE("str", tout << "building intersection for var " << var << " with automaton of size " << nfa->num_of_states() << (is_complement ? " that needs to be first complemented" : " that does not need to be first complemented") << std::endl;);
-                        if (is_complement) {
-                            nfa = std::make_shared<mata::nfa::Nfa>(mata::nfa::complement(*nfa, alph.get_mata_alphabet(), { 
-                            {"algorithm", "classical"}, 
-                            {"minimize", "true"}
-                            }));
+                    STRACE("str-mult-memb-heur",
+                        tout << "Sorted NFAs for var " << var << std::endl;
+                        unsigned i = 0;
+                        for (const auto & [is_complement, nfa] : list_of_regexes) {
+                            tout << i << " (" << (is_complement ? "" : "not ") <<"complemented):" << std::endl;
+                            tout << nfa << std::endl;
                         }
+                    );
+                    std::shared_ptr<mata::nfa::Nfa> intersection = nullptr;
+                    for (auto& [is_complement, reg] : list_of_regexes) {
+                        STRACE("str", tout << "building intersection for var " << var << " and regex " << mk_pp(reg, m) << (is_complement ? " that needs to be first complemented" : " that does not need to be first complemented") << std::endl;);
+                        
+                        std::shared_ptr<mata::nfa::Nfa> nfa = std::make_shared<mata::nfa::Nfa>(regex::conv_to_nfa(reg, m_util_s, m, alph, is_complement, is_complement));
 
                         if (intersection == nullptr) {
                             intersection = nfa;
