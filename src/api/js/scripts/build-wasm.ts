ee6dbea28b6ffca6fa691e0992cf21546554bae2
jurajsic <juraj.sic@outlook.com>
2025-01-01 23:01:07 +0100

    make it work

--- src/api/js/scripts/build-wasm.ts
+++ src/api/js/scripts/build-wasm.ts
@@ -59,12 +59,12 @@ const z3RootDir = path.join(process.cwd(), '../../../');
 
 // TODO(ritave): Detect if it's in the configuration we need
 if (!existsSync(path.join(z3RootDir, 'build/Makefile'))) {
-  spawnSync('emconfigure python scripts/mk_make.py --staticlib --single-threaded --arm64=false', {
-    cwd: z3RootDir,
+  spawnSync('emcmake cmake -DCMAKE_BUILD_TYPE=Release -DZ3_BUILD_LIBZ3_SHARED=FALSE -DZ3_SINGLE_THREADED=TRUE ..', {
+    cwd: path.join(z3RootDir, 'build'),
   });
 }
 
-spawnSync(`emmake make -j${os.cpus().length} libz3.a`, { cwd: path.join(z3RootDir, 'build') });
+spawnSync(`emmake make -j${os.cpus().length} libz3`, { cwd: path.join(z3RootDir, 'build') });
 
 const ccWrapperPath = 'build/async-fns.cc';
 console.log(`- Building ${ccWrapperPath}`);
@@ -74,8 +74,9 @@ fs.writeFileSync(ccWrapperPath, makeCCWrapper());
 const fns = JSON.stringify(exportedFuncs());
 const methods = '["PThread","ccall","FS","UTF8ToString","intArrayFromString"]';
 const libz3a = path.normalize('../../../build/libz3.a');
+const libmata = path.normalize('REPLACE THIS TEXT WITH THE PATH TO libmata.a');
 spawnSync(
-  `emcc build/async-fns.cc ${libz3a} --std=c++20 --pre-js src/low-level/async-wrapper.js -g2 -pthread -fexceptions -s WASM_BIGINT -s USE_PTHREADS=1 -s PTHREAD_POOL_SIZE=0 -s PTHREAD_POOL_SIZE_STRICT=0 -s MODULARIZE=1 -s 'EXPORT_NAME="initZ3"' -s EXPORTED_RUNTIME_METHODS=${methods} -s EXPORTED_FUNCTIONS=${fns} -s DISABLE_EXCEPTION_CATCHING=0 -s SAFE_HEAP=0 -s TOTAL_MEMORY=2GB -s TOTAL_STACK=20MB -I z3/src/api/ -o build/z3-built.js`,
+  `emcc build/async-fns.cc ${libz3a} ${libmata} --std=c++20 --pre-js src/low-level/async-wrapper.js -g2 -pthread -fexceptions -s WASM_BIGINT -s USE_PTHREADS=1 -s PTHREAD_POOL_SIZE=0 -s PTHREAD_POOL_SIZE_STRICT=0 -s MODULARIZE=1 -s 'EXPORT_NAME="initZ3"' -s EXPORTED_RUNTIME_METHODS=${methods} -s EXPORTED_FUNCTIONS=${fns} -s DISABLE_EXCEPTION_CATCHING=0 -s SAFE_HEAP=0 -s TOTAL_MEMORY=2GB -s TOTAL_STACK=20MB -I z3/src/api/ -o build/z3-built.js`,
 );
 
 fs.rmSync(ccWrapperPath);
